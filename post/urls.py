5f7e534f58777d06321a72ac15b1741bfde08503
vic-lee <viclee@nyu.edu>
2020-03-06 16:20:53 -0500

    Initialize url routing

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.urls import path
 from post import views
 
+from rest_framework_mongoengine import routers
+
+router = routers.DefaultRouter()
+router.register(r'', views.PostViewSet, basename='')
 
 urlpatterns = [
     path('ping', views.ping, name='ping'),
+    url(r'^', include(router.urls)),
 ]
