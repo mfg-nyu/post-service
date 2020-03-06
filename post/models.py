1b326b3813016927da9c8315548e19770f26c8b5
vic-lee <viclee@nyu.edu>
2020-03-06 16:18:50 -0500

    Initialize Post models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from mongoengine import Document, EmbeddedDocument, fields
+
+
+class EmbeddedFile(EmbeddedDocument):
+    filename = fields.StringField()
+    doc = fields.FileField(required=True)
+
+
+class Comments(EmbeddedDocument):
+    post_id = fields.StringField(required=True)
+    user_id = fields.StringField(required=True)
+    content = fields.StringField(required=True)
+    created_at = fields.DateTimeField(default=datetime.now())
+    file = fields.EmbeddedDocumentField(EmbeddedFile, blank=True)
+
+
+class Post(Document):
+    user_id = fields.StringField(required=True)
+    title = fields.StringField(required=True, max_length=100)
+    description = fields.StringField(default='')
+    created_at = fields.DateTimeField(default=datetime.now())
+    comments = fields.EmbeddedDocumentListField(Comments)
