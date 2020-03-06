ab5d56dc70ae7063f13817636caed363c6dd0494
vic-lee <viclee@nyu.edu>
2020-03-06 16:36:15 -0500

    Delete unused create method

--- post/views.py
+++ post/views.py
@@ -20,17 +20,3 @@ def ping(request):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.order_by('-created')
     serializer_class = PostSerializer
-
-    def create(self, request, *args, **kwargs):
-        try:
-            payload = request.data.copy()
-            serializer = self.get_serializer(data=payload)
-            serializer.is_valid(raise_exception=True)
-            resp = Post.objects.create(**serializer.validated_data)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data,
-                            status=status.HTTP_201_CREATED, headers=headers)
-
-        except Exception:
-            logger.error(traceback.format_exc())
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
